da3febed37cb39796c4df4483f326e6fb2048c8d
Евгений <borovoy@MacBook-Pro-Evgenij.local>
2024-02-07 02:45:39 +0400

    change templates and urls

--- main/views.py
+++ main/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Home",
-        "content": "Главная страница магазина - HOME"
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME"
     }
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Мы являемся крупным бла бла бла с большой историей бла бла бла"
+    }
+    return render(request, "main/about.html", context)
