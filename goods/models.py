25b07a5b38f730c568efe8aef9c1d808cdfca2b4
Евгений <borovoy@MacBook-Pro-Evgenij.local>
2024-02-13 10:34:36 +0400

    add Category and Product models

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=70, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = "category"
+
+
+
+class Products(models.Model):
+    class Meta:
+        db_table = "product"
+
+    name = models.CharField(max_length=70, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="goods_images", blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+
